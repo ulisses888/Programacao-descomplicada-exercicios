f5d304befbd23a885f9884e3432966cf2493c91d
ulisses888 <ulisses.jr2002@gmail.com>
2024-11-16 19:05:26 -0300

    Realizei o 1º exercicio

---
+++ README.md
@@ -0,0 +1,1 @@
+exercicios do site Programacao descomplicada bastante uteis para lembrar conteúdo de PC e acompanhar o conteúdo de AED1.
---
+++ exercicios: variaveis e expressoes/exercicio1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int numeroInteiro;
+    printf("\nDigite o numero inteiro:");
+    scanf("%d",&numeroInteiro);
+
+    printf("\nNumero inteiro:%d\n",numeroInteiro);
+
+    return 0;
+}
